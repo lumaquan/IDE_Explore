d4076ba2dec0046baa4f7f666fb924416eb66118
Luis Melendez MacStation <luis_melendez_mac@gmail.com>
2023-09-14 10:03:25 -0600

    Intial commit

---
+++ app/src/main/java/com/lumaquan/android/ide_explore_as3_min14_nolegacy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lumaquan.android.ide_explore_as3_min14_nolegacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
